d227ff5a9932b56ec8114c1935677905304f0394
dimka <dimonu4.1989@gmail.com>
2022-09-05 23:37:02 +0300

    ready app

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const express = require( 'express' );
+const app = express();
+const port = 4000;
+const path = require( 'path' );
 
+app.listen( port, ()=>{
+	console.log('Server is running on '+ port)
+});
+
+/*app.get( '/', (req, res)=>{
+	res.send( 'hi there ')
+})*/
+
+app.use( '/', express.static( path.join(__dirname, 'public') ));
+ 
+ 
+	
